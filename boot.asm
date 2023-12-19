e815883b8ac7caa01fa5bfe55503b195fba25c77
awe0w0 <hanser0w0@gmail.com>
2023-12-19 23:42:26 +0800

    open in vs

---
+++ boot.asm
@@ -0,0 +1,25 @@
+[org 0x7c00]
+
+;设置屏幕模式为文本模式，清楚屏幕
+
+mov ax, 3
+int 0x10
+
+mov ax, 0
+mov ds, ax
+mov es, ax
+mov ss, ax
+mov sp, 0x7c00
+
+mov ax, 0xb900
+mov ds, ax
+mov byte [0], 'H'
+
+;阻塞
+jmp $
+
+;填充 0
+times 510 - ($ - $$) db 0
+
+;主引导扇区的最后两个字节必须是 0x55 0xaa
+db 0x55, 0xaa
